c57775621358e3fadc4422fe1d4183c0d8d00ee2
Abdallah Emad <162874748+James25797@users.noreply.github.com>
2024-08-27 21:24:18 +0000

    Tue, Aug 27, 2024, 2:24 PM -07:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,1 +1,16 @@
 
+def main():
+    user_input = input("Greeting: ").strip().lower()
+    greeting(user_input)
+
+def greeting(user: str):
+    if user.split(",")[0]:
+        print("$0")
+    elif user.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+
+if __name__ == "__main__":
+    main()
